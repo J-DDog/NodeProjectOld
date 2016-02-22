e7f3c3b4c39f947ea870bc2bf96e76e6455199ab
Traitortots <jker3169@740s-pl-844.local>
2016-02-22 09:28:21 -0700

    Added Framework for CTECList in model

---
+++ src/Model/CTECList.cpp
@@ -0,0 +1,87 @@
+/*
+ * CTECList.cpp
+ *
+ *  Created on: Feb 22, 2016
+ *      Author: jker3169
+ */
+
+#include "CTECList.h"
+
+template <class Type>
+CTECList::CTECList()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+template <class Type>
+CTECList::~CTECList()
+{
+	// TODO Auto-generated destructor stub
+}
+
+template <class Type>
+int CTECList::getSize()
+{
+
+}
+
+template <class Type>
+void CTECList::addToFront(Type value)
+{
+
+}
+
+template <class Type>
+void CTECList::addToEnd(Type value)
+{
+
+}
+
+template <class Type>
+void CTECList::addAtIndex(int index, Type value)
+{
+
+}
+
+template <class Type>
+Type CTECList::getFront()
+{
+
+}
+
+template <class Type>
+Type CTECList::getEnd()
+{
+
+}
+
+template <class Type>
+Type CTECList::getFromIdex(int index)
+{
+
+}
+
+template <class Type>
+Type CTECList::removeFromFront()
+{
+
+}
+
+template <class Type>
+Type CTECList::reoveFromEnd()
+{
+
+}
+
+template <class Type>
+Type CTECList::removeFromIndex(int index)
+{
+
+}
+
+template <class Type>
+Type CTECList::set(int index, Type value)
+{
+
+}
---
+++ src/Model/CTECList.h
@@ -0,0 +1,33 @@
+/*
+ * CTECList.h
+ *
+ *  Created on: Feb 22, 2016
+ *      Author: jker3169
+ */
+
+#ifndef MODEL_CTECLIST_H_
+#define MODEL_CTECLIST_H_
+
+template <class Type>
+class CTECList {
+public:
+	CTECList();
+	~CTECList();
+	int getSize();
+	void addToFront(Type value);
+	void addToEnd(Type value);
+	void addAtIndex(int index, Type value);
+	Type getFront();
+	Type getEnd();
+	Type getFromIdex(int index);
+	Type removeFromFront();
+	Type removeFromEnd();
+	Type removeFromIndex(int index);
+	Type set(int index, Type value);
+private:
+	int size;
+	ArrayNode<Type> * head;
+	ArrayNode<Type> * end;
+};
+#endif /* MODEL_CTECLIST_H_ */
+
